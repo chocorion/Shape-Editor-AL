e649878d642b84cb6ba00a1bc381384097cfe294
rob <robiniscoding@gmail.com>
2020-03-24 09:53:26 +0100

    Add fxml view

--- src/main/java/application/ui/javafx/UiFx.java
+++ src/main/java/application/ui/javafx/UiFx.java
@@ -4,13 +4,14 @@ import application.model.Model;
 import application.model.shape.SingleShape;
 import application.ui.Ui;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 
 public class UiFx extends Application implements Ui {
@@ -38,22 +39,22 @@ public class UiFx extends Application implements Ui {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World");
-        Group root = new Group();
-        Scene scene = new Scene(root, 300, 250, Color.LIGHTGREEN);
-        Button btn = new Button();
-        btn.setLayoutX(100);
-        btn.setLayoutY(80);
-        btn.setText("Hello World");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+    public void start(final Stage primaryStage) {
+        try {
+            URL url = getClass().getClassLoader().getResource("views/mainView.fxml");
+            System.out.println("Url -> " + url);
 
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World");
-            }
-        });
-        root.getChildren().add(btn);
-        primaryStage.setScene(scene);
+            final FXMLLoader fxmlLoader = new FXMLLoader(url);
+
+            final AnchorPane root = (AnchorPane) fxmlLoader.load();
+
+            final Scene scene = new Scene(root, 300, 250);
+            primaryStage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        primaryStage.setTitle("Shape Editor");
         primaryStage.show();
     }
 
---
+++ src/main/java/application/ui/javafx/controller/mainController.java
@@ -0,0 +1,4 @@
+package application.ui.javafx.controller;
+
+public class mainController {
+}
